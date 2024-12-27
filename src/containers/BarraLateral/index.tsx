bd27b868d3a928663e562f70f131c2a00b3207f5
Lisa Mignot <lisammignot@gmail.com>
2024-12-27 13:37:06 -0300

    feat: parametrizando os filtros com o uso de Props

--- src/containers/BarraLateral/index.tsx
+++ src/containers/BarraLateral/index.tsx
@@ -7,12 +7,12 @@ const BarraLateral = () => (
     <div>
       <S.Campo type="text" placeholder="Buscar" />
       <S.Filtros>
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard />
-        <FiltroCard ativo />
+        <FiltroCard legenda="pendentes" contador={2} />
+        <FiltroCard legenda="concluídas" contador={2} />
+        <FiltroCard legenda="urgentes" contador={2} />
+        <FiltroCard legenda="importantes" contador={2} />
+        <FiltroCard legenda="normal" contador={2} />
+        <FiltroCard legenda="todas" contador={10} ativo />
       </S.Filtros>
     </div>
   </S.Aside>
