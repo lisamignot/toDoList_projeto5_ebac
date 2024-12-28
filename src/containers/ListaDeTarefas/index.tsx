66c4772bf5860161661454048bf10fd06379ef6d
Lisa Mignot <lisammignot@gmail.com>
2024-12-28 12:24:46 -0300

    feat: componentizando os botões e configurando o estado das tarefas

--- src/containers/ListaDeTarefas/index.tsx
+++ src/containers/ListaDeTarefas/index.tsx
@@ -1,24 +1,41 @@
 import Tarefa from '../../components/Tarefa'
 import { Container } from './styles'
 
+const tarefas = [
+  {
+    titulo: 'estudar',
+    descricao: 'ver aula',
+    prioridade: 'urgente',
+    status: 'pendente'
+  },
+  {
+    titulo: 'treinar',
+    descricao: 'ir à academia',
+    prioridade: 'importante',
+    status: 'concluída'
+  },
+  {
+    titulo: 'descolorir a sobrancelha',
+    descricao: 'pegar produtos',
+    prioridade: 'normal',
+    status: 'pendente'
+  }
+]
+
 const ListaDeTarefas = () => (
   <Container>
+    <p>2 tarefas marcadas como: &quot;categoria&lquot; e &quot;termo&lquot;</p>
     <ul>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
-      <li>
-        <Tarefa />
-      </li>
+      {tarefas.map((t) => (
+        <li key={t.titulo}>
+          <Tarefa
+            descricao={t.descricao}
+            titulo={t.titulo}
+            status={t.status}
+            prioridade={t.prioridade}
+          />
+        </li>
+      ))}
     </ul>
   </Container>
 )
